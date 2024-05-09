f041ea871f562bca46173318aa738ad379737575
Newtonkim <knewts98@gmail.com>
2024-05-09 19:20:32 +0300

    add logic to handle dynamic addition of items to the list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:venom_shopping_app/data/dummy_items.dart';
+import 'package:venom_shopping_app/models/grocery_item.dart';
 import 'package:venom_shopping_app/widgets/add_new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,14 +10,59 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (ctx) => const NewItem()),
-    );
-  }
+  final List<GroceryItem> _groceryItems = [];
 
+  
+  void _addItem() async{
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+        ),
+    );
+
+    if(newItem == null){
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+  void _removedItem(GroceryItem item) {
+
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
   @override
   Widget build(BuildContext context) {
+
+    Widget content = const Center(
+      child: Text('No items added yet.'),
+    );
+   if(_groceryItems.isNotEmpty) {
+      content =  ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removedItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+   }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Venom Store'),
@@ -26,20 +71,7 @@ class _GroceryListState extends State<GroceryList> {
           icon: const Icon(Icons.add))
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content
     );
   }
 }
